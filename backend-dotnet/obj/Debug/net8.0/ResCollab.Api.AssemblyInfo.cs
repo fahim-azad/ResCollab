6c445eeaad85947b8c19c895ae248fd524aff183
Fahim Azad <140386991+fahim-azad@users.noreply.github.com>
2026-09-24 15:03:01 +0600

    Build open project create/manage API

--- backend-dotnet/obj/Debug/net8.0/ResCollab.Api.AssemblyInfo.cs
+++ backend-dotnet/obj/Debug/net8.0/ResCollab.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ResCollab.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+321abbe8654553a59d42f63a99a3a242fbed6f8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac72a85395d394c0e2f9f66c909e07e15f0de1ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("ResCollab.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ResCollab.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
